ipfs/go-ipfs-archived

Implement basic filestore 'no-copy' functionality
License: MIT
Signed-off-by: Jeromy <why@ipfs.io>

filestore/filestore.go
@@ -1,0 +1,169 @@
+package filestore
+
+import (
+	"context"
+
+	"github.com/ipfs/go-ipfs/blocks"
+	"github.com/ipfs/go-ipfs/blocks/blockstore"
+	posinfo "github.com/ipfs/go-ipfs/thirdparty/posinfo"
+
+	logging "gx/ipfs/QmSpJByNKFX1sCsHBEp3R73FL4NF6FnQTEGyNAXHm2GS52/go-log"
+	cid "gx/ipfs/QmcTcsTvfaeEBRFo1TkFgT8sRmgi1n1LTZpecfVP8fzpGD/go-cid"
+)
+
+var log = logging.Logger("filestore")
+
+type Filestore struct {
+	fm *FileManager
+	bs blockstore.Blockstore
+}
+
+func NewFilestore(bs blockstore.Blockstore, fm *FileManager) *Filestore {
+	return &Filestore{fm, bs}
+}
+
+func (f *Filestore) AllKeysChan(ctx context.Context) (<-chan *cid.Cid, error) {
+	ctx, cancel := context.WithCancel(ctx)
+
+	a, err := f.bs.AllKeysChan(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	out := make(chan *cid.Cid)
+	go func() {
+		defer cancel()
+		defer close(out)
+
+		var done bool
+		for !done {
+			select {
+			case c, ok := <-a:
+				if !ok {
+					done = true
+					continue
+				}
+				select {
+				case out <- c:
+				case <-ctx.Done():
+					return
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+
+		// Can't do these at the same time because the abstractions around
+		// leveldb make us query leveldb for both operations. We apparently
+		// cant query leveldb concurrently
+		b, err := f.fm.AllKeysChan(ctx)
+		if err != nil {
+			log.Error("error querying filestore: ", err)
+			return
+		}
+
+		done = false
+		for !done {
+			select {
+			case c, ok := <-b:
+				if !ok {
+					done = true
+					continue
+				}
+				select {
+				case out <- c:
+				case <-ctx.Done():
+					return
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return out, nil
+}
+
+func (f *Filestore) DeleteBlock(c *cid.Cid) error {
+	err1 := f.bs.DeleteBlock(c)
+	if err1 != nil && err1 != blockstore.ErrNotFound {
+		return err1
+	}
+
+	if err2 := f.fm.DeleteBlock(c); err2 != nil {
+		// if we successfully removed something from the blockstore, but the
+		// filestore didnt have it, return success
+		if err1 == nil && err2 != blockstore.ErrNotFound {
+			return nil
+		}
+		return err2
+	}
+
+	return nil
+}
+
+func (f *Filestore) Get(c *cid.Cid) (blocks.Block, error) {
+	blk, err := f.bs.Get(c)
+	switch err {
+	default:
+		return nil, err
+	case nil:
+		return blk, nil
+	case blockstore.ErrNotFound:
+		// try filestore
+	}
+
+	return f.fm.Get(c)
+}
+
+func (f *Filestore) Has(c *cid.Cid) (bool, error) {
+	has, err := f.bs.Has(c)
+	if err != nil {
+		return false, err
+	}
+
+	if has {
+		return true, nil
+	}
+
+	return f.fm.Has(c)
+}
+
+func (f *Filestore) Put(b blocks.Block) error {
+	switch b := b.(type) {
+	case *posinfo.FilestoreNode:
+		return f.fm.Put(b)
+	default:
+		return f.bs.Put(b)
+	}
+}
+
+func (f *Filestore) PutMany(bs []blocks.Block) error {
+	var normals []blocks.Block
+	var fstores []*posinfo.FilestoreNode
+
+	for _, b := range bs {
+		switch b := b.(type) {
+		case *posinfo.FilestoreNode:
+			fstores = append(fstores, b)
+		default:
+			normals = append(normals, b)
+		}
+	}
+
+	if len(normals) > 0 {
+		err := f.bs.PutMany(normals)
+		if err != nil {
+			return err
+		}
+	}
+
+	if len(fstores) > 0 {
+		err := f.fm.PutMany(fstores)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var _ blockstore.Blockstore = (*Filestore)(nil)

filestore/filestore_test.go
@@ -1,0 +1,104 @@
+package filestore
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"math/rand"
+	"testing"
+
+	"github.com/ipfs/go-ipfs/blocks/blockstore"
+	dag "github.com/ipfs/go-ipfs/merkledag"
+	posinfo "github.com/ipfs/go-ipfs/thirdparty/posinfo"
+
+	ds "gx/ipfs/QmRWDav6mzWseLWeYfVd5fvUKiVe9xNH29YfMF438fG364/go-datastore"
+	cid "gx/ipfs/QmcTcsTvfaeEBRFo1TkFgT8sRmgi1n1LTZpecfVP8fzpGD/go-cid"
+)
+
+func newTestFilestore(t *testing.T) (string, *Filestore) {
+	mds := ds.NewMapDatastore()
+
+	testdir, err := ioutil.TempDir("", "filestore-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fm := NewFileManager(mds, testdir)
+
+	bs := blockstore.NewBlockstore(mds)
+	fstore := NewFilestore(bs, fm)
+	return testdir, fstore
+}
+
+func makeFile(dir string, data []byte) (string, error) {
+	f, err := ioutil.TempFile(dir, "file")
+	if err != nil {
+		return "", err
+	}
+
+	_, err = f.Write(data)
+	if err != nil {
+		return "", err
+	}
+
+	return f.Name(), nil
+}
+
+func TestBasicFilestore(t *testing.T) {
+	dir, fs := newTestFilestore(t)
+
+	buf := make([]byte, 1000)
+	rand.Read(buf)
+
+	fname, err := makeFile(dir, buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var cids []*cid.Cid
+	for i := 0; i < 100; i++ {
+		n := &posinfo.FilestoreNode{
+			PosInfo: &posinfo.PosInfo{
+				FullPath: fname,
+				Offset:   uint64(i * 10),
+			},
+			Node: dag.NewRawNode(buf[i*10 : (i+1)*10]),
+		}
+
+		err := fs.Put(n)
+		if err != nil {
+			t.Fatal(err)
+		}
+		cids = append(cids, n.Node.Cid())
+	}
+
+	for i, c := range cids {
+		blk, err := fs.Get(c)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !bytes.Equal(blk.RawData(), buf[i*10:(i+1)*10]) {
+			t.Fatal("data didnt match on the way out")
+		}
+	}
+
+	kch, err := fs.AllKeysChan(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	out := make(map[string]struct{})
+	for c := range kch {
+		out[c.KeyString()] = struct{}{}
+	}
+
+	if len(out) != len(cids) {
+		t.Fatal("mismatch in number of entries")
+	}
+
+	for _, c := range cids {
+		if _, ok := out[c.KeyString()]; !ok {
+			t.Fatal("missing cid: ", c)
+		}
+	}
+}

filestore/fsrefstore.go
@@ -1,0 +1,177 @@
+package filestore
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/ipfs/go-ipfs/blocks"
+	"github.com/ipfs/go-ipfs/blocks/blockstore"
+	pb "github.com/ipfs/go-ipfs/filestore/pb"
+	dshelp "github.com/ipfs/go-ipfs/thirdparty/ds-help"
+	posinfo "github.com/ipfs/go-ipfs/thirdparty/posinfo"
+
+	ds "gx/ipfs/QmRWDav6mzWseLWeYfVd5fvUKiVe9xNH29YfMF438fG364/go-datastore"
+	dsns "gx/ipfs/QmRWDav6mzWseLWeYfVd5fvUKiVe9xNH29YfMF438fG364/go-datastore/namespace"
+	dsq "gx/ipfs/QmRWDav6mzWseLWeYfVd5fvUKiVe9xNH29YfMF438fG364/go-datastore/query"
+	proto "gx/ipfs/QmT6n4mspWYEya864BhCUJEgyxiRfmiSY9ruQwTUNpRKaM/protobuf/proto"
+	cid "gx/ipfs/QmcTcsTvfaeEBRFo1TkFgT8sRmgi1n1LTZpecfVP8fzpGD/go-cid"
+)
+
+var FilestorePrefix = ds.NewKey("filestore")
+
+type FileManager struct {
+	ds   ds.Batching
+	root string
+}
+
+type CorruptReferenceError struct {
+	Err error
+}
+
+func (c CorruptReferenceError) Error() string {
+	return c.Err.Error()
+}
+
+func NewFileManager(ds ds.Batching, root string) *FileManager {
+	return &FileManager{dsns.Wrap(ds, FilestorePrefix), root}
+}
+
+func (f *FileManager) AllKeysChan(ctx context.Context) (<-chan *cid.Cid, error) {
+	q := dsq.Query{KeysOnly: true}
+	q.Prefix = FilestorePrefix.String()
+
+	res, err := f.ds.Query(q)
+	if err != nil {
+		return nil, err
+	}
+
+	out := make(chan *cid.Cid)
+	go func() {
+		defer close(out)
+		for {
+			v, ok := res.NextSync()
+			if !ok {
+				return
+			}
+
+			k := ds.RawKey(v.Key)
+			c, err := dshelp.DsKeyToCid(k)
+			if err != nil {
+				log.Error("decoding cid from filestore: %s", err)
+				continue
+			}
+
+			select {
+			case out <- c:
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	return out, nil
+}
+
+func (f *FileManager) DeleteBlock(c *cid.Cid) error {
+	err := f.ds.Delete(dshelp.CidToDsKey(c))
+	if err == ds.ErrNotFound {
+		return blockstore.ErrNotFound
+	}
+	return err
+}
+
+func (f *FileManager) Get(c *cid.Cid) (blocks.Block, error) {
+	o, err := f.ds.Get(dshelp.CidToDsKey(c))
+	switch err {
+	case ds.ErrNotFound:
+		return nil, blockstore.ErrNotFound
+	default:
+		return nil, err
+	case nil:
+		//
+	}
+
+	data, ok := o.([]byte)
+	if !ok {
+		return nil, fmt.Errorf("stored filestore dataobj was not a []byte")
+	}
+
+	var dobj pb.DataObj
+	if err := proto.Unmarshal(data, &dobj); err != nil {
+		return nil, err
+	}
+
+	out, err := f.readDataObj(&dobj)
+	if err != nil {
+		return nil, err
+	}
+
+	return blocks.NewBlockWithCid(out, c)
+}
+
+func (f *FileManager) readDataObj(d *pb.DataObj) ([]byte, error) {
+	abspath := filepath.Join(f.root, d.GetFilePath())
+
+	fi, err := os.Open(abspath)
+	if err != nil {
+		return nil, &CorruptReferenceError{err}
+	}
+	defer fi.Close()
+
+	_, err = fi.Seek(int64(d.GetOffset()), os.SEEK_SET)
+	if err != nil {
+		return nil, &CorruptReferenceError{err}
+	}
+
+	outbuf := make([]byte, d.GetSize_())
+	_, err = io.ReadFull(fi, outbuf)
+	if err != nil {
+		return nil, &CorruptReferenceError{err}
+	}
+
+	return outbuf, nil
+}
+
+func (f *FileManager) Has(c *cid.Cid) (bool, error) {
+	// NOTE: interesting thing to consider. Has doesnt validate the data.
+	// So the data on disk could be invalid, and we could think we have it.
+	dsk := dshelp.CidToDsKey(c)
+	return f.ds.Has(dsk)
+}
+
+func (f *FileManager) Put(b *posinfo.FilestoreNode) error {
+	var dobj pb.DataObj
+
+	if !filepath.HasPrefix(b.PosInfo.FullPath, f.root) {
+		return fmt.Errorf("cannot add filestore references outside ipfs root")
+	}
+
+	p, err := filepath.Rel(f.root, b.PosInfo.FullPath)
+	if err != nil {
+		return err
+	}
+
+	dobj.FilePath = proto.String(p)
+	dobj.Offset = proto.Uint64(b.PosInfo.Offset)
+	dobj.Size_ = proto.Uint64(uint64(len(b.RawData())))
+
+	data, err := proto.Marshal(&dobj)
+	if err != nil {
+		return err
+	}
+
+	return f.ds.Put(dshelp.CidToDsKey(b.Cid()), data)
+}
+
+func (f *FileManager) PutMany(bs []*posinfo.FilestoreNode) error {
+	// TODO: this better
+	for _, b := range bs {
+		if err := f.Put(b); err != nil {
+			return err
+		}
+	}
+	return nil
+}

ipfs/go-filestore

filestore.go
@@ -1,0 +1,169 @@
+package filestore
+
+import (
+	"context"
+
+	"github.com/ipfs/go-ipfs/blocks"
+	"github.com/ipfs/go-ipfs/blocks/blockstore"
+	posinfo "github.com/ipfs/go-ipfs/thirdparty/posinfo"
+
+	logging "gx/ipfs/QmSpJByNKFX1sCsHBEp3R73FL4NF6FnQTEGyNAXHm2GS52/go-log"
+	cid "gx/ipfs/QmV5gPoRsjN1Gid3LMdNZTyfCtP2DsvqEbMAmz82RmmiGk/go-cid"
+)
+
+var log = logging.Logger("filestore")
+
+type Filestore struct {
+	fm *FileManager
+	bs blockstore.Blockstore
+}
+
+func NewFilestore(bs blockstore.Blockstore, fm *FileManager) *Filestore {
+	return &Filestore{fm, bs}
+}
+
+func (f *Filestore) AllKeysChan(ctx context.Context) (<-chan *cid.Cid, error) {
+	ctx, cancel := context.WithCancel(ctx)
+
+	a, err := f.bs.AllKeysChan(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	out := make(chan *cid.Cid)
+	go func() {
+		defer cancel()
+		defer close(out)
+
+		var done bool
+		for !done {
+			select {
+			case c, ok := <-a:
+				if !ok {
+					done = true
+					continue
+				}
+				select {
+				case out <- c:
+				case <-ctx.Done():
+					return
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+
+		// Can't do these at the same time because the abstractions around
+		// leveldb make us query leveldb for both operations. We apparently
+		// cant query leveldb concurrently
+		b, err := f.fm.AllKeysChan(ctx)
+		if err != nil {
+			log.Error("error querying filestore: ", err)
+			return
+		}
+
+		done = false
+		for !done {
+			select {
+			case c, ok := <-b:
+				if !ok {
+					done = true
+					continue
+				}
+				select {
+				case out <- c:
+				case <-ctx.Done():
+					return
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+	return out, nil
+}
+
+func (f *Filestore) DeleteBlock(c *cid.Cid) error {
+	err1 := f.bs.DeleteBlock(c)
+	if err1 != nil && err1 != blockstore.ErrNotFound {
+		return err1
+	}
+
+	if err2 := f.fm.DeleteBlock(c); err2 != nil {
+		// if we successfully removed something from the blockstore, but the
+		// filestore didnt have it, return success
+		if err1 == nil && err2 != blockstore.ErrNotFound {
+			return nil
+		}
+		return err2
+	}
+
+	return nil
+}
+
+func (f *Filestore) Get(c *cid.Cid) (blocks.Block, error) {
+	blk, err := f.bs.Get(c)
+	switch err {
+	default:
+		return nil, err
+	case nil:
+		return blk, nil
+	case blockstore.ErrNotFound:
+		// try filestore
+	}
+
+	return f.fm.Get(c)
+}
+
+func (f *Filestore) Has(c *cid.Cid) (bool, error) {
+	has, err := f.bs.Has(c)
+	if err != nil {
+		return false, err
+	}
+
+	if has {
+		return true, nil
+	}
+
+	return f.fm.Has(c)
+}
+
+func (f *Filestore) Put(b blocks.Block) error {
+	switch b := b.(type) {
+	case *posinfo.FilestoreNode:
+		return f.fm.Put(b)
+	default:
+		return f.bs.Put(b)
+	}
+}
+
+func (f *Filestore) PutMany(bs []blocks.Block) error {
+	var normals []blocks.Block
+	var fstores []*posinfo.FilestoreNode
+
+	for _, b := range bs {
+		switch b := b.(type) {
+		case *posinfo.FilestoreNode:
+			fstores = append(fstores, b)
+		default:
+			normals = append(normals, b)
+		}
+	}
+
+	if len(normals) > 0 {
+		err := f.bs.PutMany(normals)
+		if err != nil {
+			return err
+		}
+	}
+
+	if len(fstores) > 0 {
+		err := f.fm.PutMany(fstores)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var _ blockstore.Blockstore = (*Filestore)(nil)

filestore_test.go
@@ -1,0 +1,104 @@
+package filestore
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"math/rand"
+	"testing"
+
+	"github.com/ipfs/go-ipfs/blocks/blockstore"
+	dag "github.com/ipfs/go-ipfs/merkledag"
+	posinfo "github.com/ipfs/go-ipfs/thirdparty/posinfo"
+
+	ds "gx/ipfs/QmRWDav6mzWseLWeYfVd5fvUKiVe9xNH29YfMF438fG364/go-datastore"
+	cid "gx/ipfs/QmV5gPoRsjN1Gid3LMdNZTyfCtP2DsvqEbMAmz82RmmiGk/go-cid"
+)
+
+func newTestFilestore(t *testing.T) (string, *Filestore) {
+	mds := ds.NewMapDatastore()
+
+	testdir, err := ioutil.TempDir("", "filestore-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fm := NewFileManager(mds, testdir)
+
+	bs := blockstore.NewBlockstore(mds)
+	fstore := NewFilestore(bs, fm)
+	return testdir, fstore
+}
+
+func makeFile(dir string, data []byte) (string, error) {
+	f, err := ioutil.TempFile(dir, "file")
+	if err != nil {
+		return "", err
+	}
+
+	_, err = f.Write(data)
+	if err != nil {
+		return "", err
+	}
+
+	return f.Name(), nil
+}
+
+func TestBasicFilestore(t *testing.T) {
+	dir, fs := newTestFilestore(t)
+
+	buf := make([]byte, 1000)
+	rand.Read(buf)
+
+	fname, err := makeFile(dir, buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var cids []*cid.Cid
+	for i := 0; i < 100; i++ {
+		n := &posinfo.FilestoreNode{
+			PosInfo: &posinfo.PosInfo{
+				FullPath: fname,
+				Offset:   uint64(i * 10),
+			},
+			Node: dag.NewRawNode(buf[i*10 : (i+1)*10]),
+		}
+
+		err := fs.Put(n)
+		if err != nil {
+			t.Fatal(err)
+		}
+		cids = append(cids, n.Node.Cid())
+	}
+
+	for i, c := range cids {
+		blk, err := fs.Get(c)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !bytes.Equal(blk.RawData(), buf[i*10:(i+1)*10]) {
+			t.Fatal("data didnt match on the way out")
+		}
+	}
+
+	kch, err := fs.AllKeysChan(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	out := make(map[string]struct{})
+	for c := range kch {
+		out[c.KeyString()] = struct{}{}
+	}
+
+	if len(out) != len(cids) {
+		t.Fatal("mismatch in number of entries")
+	}
+
+	for _, c := range cids {
+		if _, ok := out[c.KeyString()]; !ok {
+			t.Fatal("missing cid: ", c)
+		}
+	}
+}

fsrefstore.go
@@ -1,0 +1,177 @@
+package filestore
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/ipfs/go-ipfs/blocks"
+	"github.com/ipfs/go-ipfs/blocks/blockstore"
+	pb "github.com/ipfs/go-ipfs/filestore/pb"
+	dshelp "github.com/ipfs/go-ipfs/thirdparty/ds-help"
+	posinfo "github.com/ipfs/go-ipfs/thirdparty/posinfo"
+
+	ds "gx/ipfs/QmRWDav6mzWseLWeYfVd5fvUKiVe9xNH29YfMF438fG364/go-datastore"
+	dsns "gx/ipfs/QmRWDav6mzWseLWeYfVd5fvUKiVe9xNH29YfMF438fG364/go-datastore/namespace"
+	dsq "gx/ipfs/QmRWDav6mzWseLWeYfVd5fvUKiVe9xNH29YfMF438fG364/go-datastore/query"
+	proto "gx/ipfs/QmT6n4mspWYEya864BhCUJEgyxiRfmiSY9ruQwTUNpRKaM/protobuf/proto"
+	cid "gx/ipfs/QmV5gPoRsjN1Gid3LMdNZTyfCtP2DsvqEbMAmz82RmmiGk/go-cid"
+)
+
+var FilestorePrefix = ds.NewKey("filestore")
+
+type FileManager struct {
+	ds   ds.Batching
+	root string
+}
+
+type CorruptReferenceError struct {
+	Err error
+}
+
+func (c CorruptReferenceError) Error() string {
+	return c.Err.Error()
+}
+
+func NewFileManager(ds ds.Batching, root string) *FileManager {
+	return &FileManager{dsns.Wrap(ds, FilestorePrefix), root}
+}
+
+func (f *FileManager) AllKeysChan(ctx context.Context) (<-chan *cid.Cid, error) {
+	q := dsq.Query{KeysOnly: true}
+	q.Prefix = FilestorePrefix.String()
+
+	res, err := f.ds.Query(q)
+	if err != nil {
+		return nil, err
+	}
+
+	out := make(chan *cid.Cid)
+	go func() {
+		defer close(out)
+		for {
+			v, ok := res.NextSync()
+			if !ok {
+				return
+			}
+
+			k := ds.RawKey(v.Key)
+			c, err := dshelp.DsKeyToCid(k)
+			if err != nil {
+				log.Error("decoding cid from filestore: %s", err)
+				continue
+			}
+
+			select {
+			case out <- c:
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	return out, nil
+}
+
+func (f *FileManager) DeleteBlock(c *cid.Cid) error {
+	err := f.ds.Delete(dshelp.CidToDsKey(c))
+	if err == ds.ErrNotFound {
+		return blockstore.ErrNotFound
+	}
+	return err
+}
+
+func (f *FileManager) Get(c *cid.Cid) (blocks.Block, error) {
+	o, err := f.ds.Get(dshelp.CidToDsKey(c))
+	switch err {
+	case ds.ErrNotFound:
+		return nil, blockstore.ErrNotFound
+	default:
+		return nil, err
+	case nil:
+		//
+	}
+
+	data, ok := o.([]byte)
+	if !ok {
+		return nil, fmt.Errorf("stored filestore dataobj was not a []byte")
+	}
+
+	var dobj pb.DataObj
+	if err := proto.Unmarshal(data, &dobj); err != nil {
+		return nil, err
+	}
+
+	out, err := f.readDataObj(&dobj)
+	if err != nil {
+		return nil, err
+	}
+
+	return blocks.NewBlockWithCid(out, c)
+}
+
+func (f *FileManager) readDataObj(d *pb.DataObj) ([]byte, error) {
+	abspath := filepath.Join(f.root, d.GetFilePath())
+
+	fi, err := os.Open(abspath)
+	if err != nil {
+		return nil, &CorruptReferenceError{err}
+	}
+	defer fi.Close()
+
+	_, err = fi.Seek(int64(d.GetOffset()), os.SEEK_SET)
+	if err != nil {
+		return nil, &CorruptReferenceError{err}
+	}
+
+	outbuf := make([]byte, d.GetSize_())
+	_, err = io.ReadFull(fi, outbuf)
+	if err != nil {
+		return nil, &CorruptReferenceError{err}
+	}
+
+	return outbuf, nil
+}
+
+func (f *FileManager) Has(c *cid.Cid) (bool, error) {
+	// NOTE: interesting thing to consider. Has doesnt validate the data.
+	// So the data on disk could be invalid, and we could think we have it.
+	dsk := dshelp.CidToDsKey(c)
+	return f.ds.Has(dsk)
+}
+
+func (f *FileManager) Put(b *posinfo.FilestoreNode) error {
+	var dobj pb.DataObj
+
+	if !filepath.HasPrefix(b.PosInfo.FullPath, f.root) {
+		return fmt.Errorf("cannot add filestore references outside ipfs root")
+	}
+
+	p, err := filepath.Rel(f.root, b.PosInfo.FullPath)
+	if err != nil {
+		return err
+	}
+
+	dobj.FilePath = proto.String(p)
+	dobj.Offset = proto.Uint64(b.PosInfo.Offset)
+	dobj.Size_ = proto.Uint64(uint64(len(b.RawData())))
+
+	data, err := proto.Marshal(&dobj)
+	if err != nil {
+		return err
+	}
+
+	return f.ds.Put(dshelp.CidToDsKey(b.Cid()), data)
+}
+
+func (f *FileManager) PutMany(bs []*posinfo.FilestoreNode) error {
+	// TODO: this better
+	for _, b := range bs {
+		if err := f.Put(b); err != nil {
+			return err
+		}
+	}
+	return nil
+}